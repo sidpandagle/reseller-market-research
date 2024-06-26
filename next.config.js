c4c8bab425fb911b0b99c28864f56c7595bc584e
bluesalt <siddhant.pandagle1998@gmail.com>
2024-06-26 23:33:46 +0530

    API URL changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        apiUrl : 'https://reseller-backend-production.up.railway.app',
+        // apiUrl : 'https://reseller-backend-production.up.railway.app',
+        apiUrl : 'https://api.researchenvision.com',
         reCaptchaKey : '6Ldkc_8pAAAAAAm3WhyI2BuckwLlIScoxgN0KdtN'
     }
 }
