b42842dc05c29a32e3d03292684536d878d31ca2
fudge-fantastic <siddhant.pandagle1998@gmail.com>
2024-08-08 23:17:58 +0530

    Disallowed not-required report pages

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,12 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: [
+        '/report/*/toc',
+        '/report/*/highlights',
+        '/report/*/methodology',
+        '/report/*/request-sample'
+      ]
     },
     sitemap: 'https://www.researchenvision.com/sitemap.xml',
   }
